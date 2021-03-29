1e1e8f169da2c98081acbbcf1d3474027cd80916
chuanqizhang <legend.z@qq.com>
2021-03-29 16:58:48 +0800

    fix typo in init-dsid.sh

--- rootfsimg/init-dsid.sh
+++ rootfsimg/init-dsid.sh
@@ -6,4 +6,4 @@ mount -t cgroup -o dsid dsid /sys/fs/cgroup/dsid
 mkdir /sys/fs/cgroup/dsid/test-1 /sys/fs/cgroup/dsid/test-2
 
 echo 1 > /sys/fs/cgroup/dsid/test-1/dsid.dsid-set
-echo 2 > /sys/fs/cgroup/dsid/test-1/dsid.dsid-set
+echo 2 > /sys/fs/cgroup/dsid/test-2/dsid.dsid-set
